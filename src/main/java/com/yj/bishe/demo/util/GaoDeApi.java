05c3e570018d6c6df1e3e0531a9702f2e5b96b91
YJBoss <1055658601@qq.com>
2021-02-20 18:53:04 +0800

    导航栏完成一部分了，还有登陆和未登录的动态没做，高德地图定位接口写好了，采用后端调用接口的方式，减少系统可见性

---
+++ src/main/java/com/yj/bishe/demo/util/GaoDeApi.java
@@ -0,0 +1,62 @@
+package com.yj.bishe.demo.util;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 直接通过后端调用高德web接口，不让前端页面可以看到
+ */
+public class GaoDeApi {
+
+    public static String SendGET(String url,String param){
+        StringBuilder result= new StringBuilder();//访问返回结果
+        BufferedReader read=null;//读取访问结果
+
+        try {
+            //创建url
+            URL realurl=new URL(url+"?"+param);
+            //打开连接
+            URLConnection connection=realurl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            //建立连接
+            connection.connect();
+            // 获取所有响应头字段bai
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段，获取到cookies等
+//            for (String key : map.keySet()) {
+//                System.out.println(key + "--->" + map.get(key));
+//            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            read = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream(), StandardCharsets.UTF_8));
+            String line;//循环读取
+            while ((line = read.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            if(read!=null){//关闭流
+                try {
+                    read.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result.toString();
+    }
+
+}
--- src/main/java/com/yj/bishe/demo/web/AddressController.java
+++ src/main/java/com/yj/bishe/demo/web/AddressController.java
@@ -1,9 +1,12 @@
 package com.yj.bishe.demo.web;
 
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.alibaba.fastjson.JSONObject;
+import com.yj.bishe.demo.util.GaoDeApi;
+import com.yj.bishe.demo.vo.JsonResult;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -14,7 +17,27 @@ import org.springframework.stereotype.Controller;
  * @since 2021-02-14
  */
 @Controller
-@RequestMapping("//address")
+//@RequestMapping("//address")
 public class AddressController {
+
+    //高德地图web调用的密钥
+    final String key = "36a678df773856fff019cc42ebfc80c0";
+
+    //根据前端传来的地址经纬度查询出具体地址信息
+    @PostMapping("/getaddress")
+    @ResponseBody
+    public JSONObject getAddressByGaoDe(String URL,String param){
+        JsonResult ret;
+        param = "key="+key+"&location="+param;
+        String sendGET = GaoDeApi.SendGET(URL, param);
+        JSONObject jsonObject = JSONObject.parseObject(sendGET);
+        return jsonObject;
+    }
+
+    //关键字查询
+
+
+
+
 
 }
