d73d46c13fdf9dd3efea6ab55df5361af458758f
YJBoss <1055658601@qq.com>
2021-03-28 22:41:59 +0800

    统一404跳转网页

--- src/main/java/com/yj/bishe/demo/service/IFavoService.java
+++ src/main/java/com/yj/bishe/demo/service/IFavoService.java
@@ -14,7 +14,8 @@ import com.yj.bishe.demo.vo.JsonResult;
  */
 public interface IFavoService extends IService<Favo> {
 
-    //收藏房源
+    //收藏房源,要考虑到用户重复点击收藏按钮的情况
+    //所以要先进行判断，用uid和lid
     JsonResult favoritesByLid(Integer uid, Integer lid);
 
 }
--- src/main/java/com/yj/bishe/demo/service/impl/FavoServiceImpl.java
+++ src/main/java/com/yj/bishe/demo/service/impl/FavoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.yj.bishe.demo.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yj.bishe.demo.dao.ListingsMapper;
 import com.yj.bishe.demo.entity.Favo;
 import com.yj.bishe.demo.dao.FavoMapper;
 import com.yj.bishe.demo.service.IFavoService;
@@ -23,10 +25,32 @@ public class FavoServiceImpl extends ServiceImpl<FavoMapper, Favo> implements IF
     @Resource
     FavoMapper favoMapper;
 
+    @Resource
+    ListingsMapper listingsMapper;
+
     @Override
     public JsonResult favoritesByLid(Integer uid, Integer lid) {
-        JsonResult ret = null;
-
+        JsonResult ret;
+        QueryWrapper<Favo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("uid",uid);
+        queryWrapper.eq("lid",lid);
+        Favo selectOne = favoMapper.selectOne(queryWrapper);
+        if (selectOne == null) {
+            Integer aid = listingsMapper.selectById(lid).getAid();
+            if (aid != 0) {
+                Favo favo = new Favo();
+                favo.setUid(uid);
+                favo.setLid(lid);
+                favo.setAid(aid);
+                int insert = favoMapper.insert(favo);
+                if (insert == 1)
+                    ret = new JsonResult(true, "房源收藏成功");
+                else
+                    ret = new JsonResult(false, "房源收藏失败");
+            } else
+                ret = new JsonResult(false, "房源收藏失败，房源地址异常");
+        }else
+            ret = new JsonResult(false,"您已经收藏过该房源了");
         return ret;
     }
 }
